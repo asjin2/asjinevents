da7078d25c9724382969cd93a019481a05aa9831
asjin2 <asjin.private@gmail.com>
2025-08-25 11:29:17 +0530

    Fix Google Maps embed URL for service area

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -271,7 +271,16 @@ Please contact this customer for wedding decoration services.`;
 
           <div className="bg-white p-4 rounded-2xl shadow-lg">
             <div className="aspect-w-16 aspect-h-9">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1974.5216427147955!2d77.3010467!3d8.1983943!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5d4d656a7b7e81f%3A0x2603a640b2a7dc1b!2sAsjin%20Events!5e0!3m2!1sen!2sin!4v1756101368738!5m2!1sen!2sin" width="600" height="450" style={{ border: 0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125663.89668275378!2d77.35104675!3d8.1774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b04f0c0c0c0c0c0%3A0x0!2sNagercoil%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1640000000000!5m2!1sen!2sin"
+                width="100%" 
+                height="450" 
+                style={{ border: 0 }} 
+                allowFullScreen 
+                loading="lazy" 
+                referrerPolicy="no-referrer-when-downgrade"
+                title="Asjin Events Service Area - Nagercoil and Kanyakumari District"
+              />
             </div>
           </div>
         </div>
