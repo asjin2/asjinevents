8dfbbb540e622385763bda8977569f5856fd71fa
asjin2 <asjin.private@gmail.com>
2025-08-25 11:30:23 +0530

    Replace Google Maps with working embed URL

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -272,7 +272,7 @@ Please contact this customer for wedding decoration services.`;
           <div className="bg-white p-4 rounded-2xl shadow-lg">
             <div className="aspect-w-16 aspect-h-9">
               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125663.89668275378!2d77.35104675!3d8.1774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b04f0c0c0c0c0c0%3A0x0!2sNagercoil%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1640000000000!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125663.89668275378!2d77.35104675!3d8.1774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b04f1c1c1c1c1c1%3A0x1c1c1c1c1c1c1c1c!2sNagercoil%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1640000000000!5m2!1sen!2sin"
                 width="100%" 
                 height="450" 
                 style={{ border: 0 }} 
